8a122ae3b1f87c3ff84bc82000dc2c3b3f9344a2
PLN15\Administrador <pedroboaventura05@gmail.com>
2023-03-06 11:24:34 +0000

    atualizaçao 06/03

--- src/Calcoes.js
+++ src/Calcoes.js
@@ -2,16 +2,179 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import { Link } from "react-router-dom";
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Figure from 'react-bootstrap/Figure';
 
 function Calcoes() {
-    return (
-      <div className="App">
+
+  var data = require('./Calcoes.json');
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
       <body className='App-body'>
-      <p>Calções!</p>
+        <ul>
+          <Container>
+            <Row xs={1} md={4}>
+              {
+                data.Adidas.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.Antix.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.Carhartt.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.Dickies.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.Nike.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.Rell.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+            </Row>
+          </Container>
+        </ul>
       </body>
+      <footer>
+
+      </footer>
     </div>
-      
-    );
-  }
-  
-  export default Calcoes;
+  );
+}
+
+export default Calcoes;
--- src/Sapatos.js
+++ src/Sapatos.js
@@ -2,16 +2,155 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import { Link } from "react-router-dom";
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Figure from 'react-bootstrap/Figure';
 
 function Sapatos() {
-    return (
-      <div className="App">
+
+  var data = require('./Sapatos.json');
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
       <body className='App-body'>
-      <p>Sapatos!</p>
+        <ul>
+          <Container>
+            <Row xs={1} md={4}>
+              {
+                data.Adidas.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.DC.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.NewBalance.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.Nike.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+              {
+                data.Vans.map(item => (
+                  <li key={item.id}>
+                    <Figure>
+                      <Figure.Image
+                        width={250}
+                        height={250}
+                        src={item.imgsrc}
+                      />
+                      <Figure.Caption>
+                      </Figure.Caption>
+                    </Figure>
+                    <h1>
+                      {item.brand}
+                    </h1>
+                    <h5>
+                      {item.model}
+                    </h5>
+                    <p2>
+                      {item.price}
+                    </p2>
+                  </li>
+                ))
+              }
+            </Row>
+          </Container>
+        </ul>
       </body>
+      <footer>
+
+      </footer>
     </div>
-      
-    );
-  }
-  
-  export default Sapatos;
+  );
+}
+
+export default Sapatos;
