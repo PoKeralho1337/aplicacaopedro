5cf66cc61c63a54e591f34b4a110a30a52558879
Pedro <pedroboaventura05@gmail.com>
2023-06-15 20:22:29 +0100

    marco

--- src/Home.js
+++ src/Home.js
@@ -12,11 +12,11 @@ import Col from 'react-bootstrap/Col';
 import tabua1 from "./img/tabua1.png"
 import tabua2 from "./img/tabua2.png"
 import tabua3 from "./img/tabua3.png"
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import tabua4 from "./img/tabua4.png"
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
 
 
-
-const images = [tabua1, tabua2, tabua3]
+const images = [tabua1, tabua2, tabua3, tabua4];
 
 function Home() {
   const sectionsRef = useRef([]);
@@ -25,7 +25,7 @@ function Home() {
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.3, 
+      threshold: 0.3,
     };
 
     const observer = new IntersectionObserver((entries) => {
@@ -48,34 +48,38 @@ function Home() {
   }, []);
 
 
-  const NextArrow = ({onClick}) => {
-    return(
-      <div className='arrow next' onClick={onClick}>
-        
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight/>
       </div>
-    )
-  }
+    );
+  };
 
-  const PrevArrow = ({onClick}) => {
-    return(
-      <div className='arrow prev' onClick={onClick}>
-        
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft/>
       </div>
-    )
-  }
+    );
+  };
+
+  const [imageIndex, setImageIndex] = useState(0);
 
   const settings = {
     infinite: true,
-    lazyload: true,
+    lazyLoad: true,
     speed: 300,
     slidesToShow: 3,
     centerMode: true,
     centerPadding: 0,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />
-  }
+    nextArrow: <NextArrow/>,
+    prevArrow: <PrevArrow/>,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+
   return (
-    <div className="App">
+    <div className="">
       <header className="App-header">
         <link rel="stylesheet"
           href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" />
@@ -89,7 +93,7 @@ function Home() {
         <section ref={(el) => (sectionsRef.current[0] = el)} className="home fade-in">
           <div class="circle"></div>
           <div class="home-text">
-            <span>Bem-Vindo Ao</span> 
+            <span>Bem-Vindo Ao</span>
             <h1>EspoSkate</h1>
             <h2>Melhor Loja Online<br></br>De Skates</h2>
             <a href='' className="my-btn">Continuar</a>
@@ -102,135 +106,138 @@ function Home() {
           <div class="center-text">
             <h1>Nova <span>Coleção</span></h1>
           </div>
-
           <div className="produtos">
             <Container>
-            <Row className="row-2 img">
-              <Col>
-              <table>
-            <div className='produto-card'>
-            <div class="card__infos">
-              <img src='imagens/adidascasacos3.png' alt=""/>
-              <div class="product-text">
-              </div>
-            </div>
-            <div class="card__title">
-              <h4>Casaco Adidas</h4>
-            </div>
-            <div class="card__price">
-              <p>76$</p>
-            </div>
-            </div>
-            </table>
-            </Col>
-            <Col>
-            <table>
-            <div className='produto-card'>
-            <div class="card__infos">
-              <img src='imagens/antixcalcas2.png' alt=""/>
-              <div class="product-text">
-              </div>
-            </div>
-            <div class="card__title">
-              <h4>Calcas Antix</h4>
-            </div>
-            <div class="card__price">
-              <p>80$</p>
-            </div>
-            </div>
-            </table>
-            </Col>
-            <Col>
-            <table>
-            <div className='produto-card'>
-            <div class="card__infos">
-              <img src='imagens/rellcalcoes1.png' alt=""/>
-              <div class="product-text">
-              </div>
-            </div>
-            <div class="card__title">
-              <h4>Sapatos Nike</h4>
-            </div>
-            <div class="card__price">
-              <p>60$</p>
-            </div>
-            </div>
-            </table>
-            </Col>
-            </Row>
-            <Row>
-            <Col>
-            <table>
-            <div className='produto-card'>
-            <div class="card__infos">
-              <img src='imagens/thrasherhoodie14.png'  alt="" class="produto-imagem"/>
-              <div class="product-text">
-              </div>
-            </div>
-            <div class="card__title">
-              <h4>Tábua Jart</h4>
-            </div>
-            <div class="card__price card__bottom ">
-              <p>120$</p>
-            </div>
-            </div>
-            </table>
-            </Col>
-            <Col>
-            <table>
-            <div className='produto-card'>
-            <div class="card__infos">
-              <img src='imagens/nikechapeus1.png'  alt="" class="produto-imagem"/>
-              <div class="product-text">
-              </div>
-            </div>
-            <div class="card__title">
-              <h4>Tábua Jart</h4>
-            </div>
-            <div class="card__price card__bottom ">
-              <p>120$</p>
-            </div>
-            </div>
-            </table>
-            </Col>
-            <Col>
-            <table>
-            <div className='produto-card'>
-            <div class="card__infos">
-              <img src='imagens/vanssapatos1.png' />
-              <div class="product-text">
-              </div>
-            </div>
-            <div class="card__title">
-              <h4>Tábua Jart</h4>
-            </div>
-            <div class="card__price card__bottom " >
-              <p>120$</p>
-            </div>
-            </div>
-            </table>
-            </Col>
-            </Row>
-            </Container>       
+              <Row className="row-2 img">
+                <Col>
+                  <table>
+                    <div className='produto-card'>
+                      <div class="card__infos">
+                        <img src='imagens/adidascasacos3.png' alt="" />
+                        <div class="product-text">
+                        </div>
+                      </div>
+                      <div class="card__title">
+                        <h4>Casaco Adidas</h4>
+                      </div>
+                      <div class="card__price">
+                        <p>76$</p>
+                      </div>
+                    </div>
+                  </table>
+                </Col>
+                <Col>
+                  <table>
+                    <div className='produto-card'>
+                      <div class="card__infos">
+                        <img src='imagens/antixcalcas2.png' alt="" />
+                        <div class="product-text">
+                        </div>
+                      </div>
+                      <div class="card__title">
+                        <h4>Calcas Antix</h4>
+                      </div>
+                      <div class="card__price">
+                        <p>80$</p>
+                      </div>
+                    </div>
+                  </table>
+                </Col>
+                <Col>
+                  <table>
+                    <div className='produto-card'>
+                      <div class="card__infos">
+                        <img src='imagens/rellcalcoes1.png' alt="" />
+                        <div class="product-text">
+                        </div>
+                      </div>
+                      <div class="card__title">
+                        <h4>Sapatos Nike</h4>
+                      </div>
+                      <div class="card__price">
+                        <p>60$</p>
+                      </div>
+                    </div>
+                  </table>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <table>
+                    <div className='produto-card'>
+                      <div class="card__infos">
+                        <img src='imagens/thrasherhoodie14.png' alt="" class="produto-imagem" />
+                        <div class="product-text">
+                        </div>
+                      </div>
+                      <div class="card__title">
+                        <h4>Tábua Jart</h4>
+                      </div>
+                      <div class="card__price card__bottom ">
+                        <p>120$</p>
+                      </div>
+                    </div>
+                  </table>
+                </Col>
+                <Col>
+                  <table>
+                    <div className='produto-card'>
+                      <div class="card__infos">
+                        <img src='imagens/nikechapeus1.png' alt="" class="produto-imagem" />
+                        <div class="product-text">
+                        </div>
+                      </div>
+                      <div class="card__title">
+                        <h4>Tábua Jart</h4>
+                      </div>
+                      <div class="card__price card__bottom ">
+                        <p>120$</p>
+                      </div>
+                    </div>
+                  </table>
+                </Col>
+                <Col>
+                  <table>
+                    <div className='produto-card'>
+                      <div class="card__infos">
+                        <img src='imagens/vanssapatos1.png' />
+                        <div class="product-text">
+                        </div>
+                      </div>
+                      <div class="card__title">
+                        <h4>Tábua Jart</h4>
+                      </div>
+                      <div class="card__price card__bottom " >
+                        <p>120$</p>
+                      </div>
+                    </div>
+                  </table>
+                </Col>
+              </Row>
+            </Container>
           </div>
         </section>
 
-        <section>
-          <div className='carousel'>
+        <section ref={(el) => (sectionsRef.current[2] = el)} className="trending-product fade-in">
+        <div class="center-text">
+            <h1>Promoções</h1>
+          </div>
+          <div className="carosel">
             <Slider {...settings}>
               {images.map((img, idx) => (
-                <div>
+                <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
                   <img src={img} alt={img} />
                 </div>
               ))}
             </Slider>
           </div>
-          </section>
+        </section>
+
 
         <section class="contact">
           <div class="contact-info">
             <div class="first-info">
-              <img src='/imagens/EspoSkate.png'/> <br></br>
+              <img src='/imagens/EspoSkate.png' /> <br></br>
 
               <a href='https://www.google.com/maps/place/C%C3%A2mara+Municipal+de+Esposende/@41.5314358,-8.7801767,18.96z/data=!4m14!1m7!3m6!1s0xd244bba6d8c44a1:0xd3bfe8a5d9eb0d42!2sC%C3%A2mara+Municipal+de+Esposende!8m2!3d41.531351!4d-8.7806344!16s%2Fg%2F1tfjmdhm!3m5!1s0xd244bba6d8c44a1:0xd3bfe8a5d9eb0d42!8m2!3d41.531351!4d-8.7806344!16s%2Fg%2F1tfjmdhm'>Praça do Município,<br />4740-223 Esposende</a>
               <p>966 597 688</p>
